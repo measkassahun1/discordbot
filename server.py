dcb399543fd34de3ff5d42e8a9f6f33839e0c15a
measkassahun1 <mkassahun@sea.yearup.org>
2024-06-14 01:16:14 -0700

    intents

--- server.py
+++ server.py
@@ -1,23 +1,27 @@
-#Class Libraries 
+# Class Libraries
 import discord
 import os
-import random 
+import random
 from dotenv import load_dotenv
 from ec2_metadata import ec2_metadata
 
-
-#Initializing Variables
+# Initializing Variables
 load_dotenv()
 
-client = discord.Client()
+# Define intents
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Initialize the client with intents
+client = discord.Client(intents=intents)
 token = os.getenv('TOKEN')
 
-#Initializing Bot
+# Initializing Bot
 @client.event
 async def on_ready():
     print("Logged in as a bot {0.user}".format(client))
 
-#Setting up bot responses
+# Setting up bot responses
 @client.event
 async def on_message(message):
     username = str(message.author).split('#')[0]
@@ -32,18 +36,14 @@ async def on_message(message):
     if channel == "random":
         if user_message.lower() == "hello" or user_message.lower() == "hi":
             await message.channel.send(f'Hi {username}')
-        #Other string options
         elif user_message.lower() == "ring":
             await message.channel.send(f'Green green yellow {username}')
-        #Other string options
         elif user_message.lower() == "hey pookie":
-            await user_message.channel.send(f'Purr {username}')
-        #Returning instance data
+            await message.channel.send(f'Purr {username}')
         elif user_message.lower() == "ec2 data":
             await message.channel.send(f'Your instance data is {ec2_metadata.instance} Your EC2 Data: {ec2_metadata.region}')
         else:
             await message.channel.send("Error")
 
-#Run the bot w/token
+# Run the bot with token
 client.run(token)
-
